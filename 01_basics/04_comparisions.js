2f2201a2fab4beba58485dad248d1e9fa3c72254
tanyaboraa <tanya.20243291@mnnit.ac.in>
2026-02-01 13:16:18 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2!=1);
+
+
+// console.log("2">1); //true
+// console.log("02">1); //true
+
+//AVOID-
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);  // false wherreas ans is true so answer not predictable 
+
+// console.log(undefined==0); // different from < and <=
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//=== strictly check(with datatypes)
+
+console.log("2"===2)
