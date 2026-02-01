3c914b7e58993d5a37c0967248fb9ef0c8ab6e41
tanyaboraa <tanya.20243291@mnnit.ac.in>
2026-02-01 12:41:27 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score="33abc"
+// console.log(typeof(score))
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber) //NaN
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true=> 1
+
+let isLoggedIn ="t"
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn )
+
+// 1=> true , 0=> false
+// "" => false
+// "tanya" => true
+
+let someNumber= 33
+let stringNumber= String(someNumber)
+ 
+console.log(stringNumber)
+console.log(typeof stringNumber)
