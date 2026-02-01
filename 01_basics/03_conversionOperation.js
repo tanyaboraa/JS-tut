c9e1ba66558222462ff9d41c2f2e8c9811a7fac1
tanyaboraa <tanya.20243291@mnnit.ac.in>
2026-02-01 12:56:02 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -10,16 +10,53 @@
 // "33abc" => NaN (not a number)
 // true=> 1
 
-let isLoggedIn ="t"
-let booleanIsLoggedIn =Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn )
+// let isLoggedIn ="t"
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn )
 
 // 1=> true , 0=> false
 // "" => false
 // "tanya" => true
 
-let someNumber= 33
-let stringNumber= String(someNumber)
+// let someNumber= 33
+// let stringNumber= String(someNumber)
  
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// ****************** OPERATIONS**************
+
+// let value=3
+// let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1="hello"
+// let str2=" tanya"
+// let str3=str1+str2
+// console.log(str3)
+
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+
+// console.log(+true)
+// console.log(+""); //0
+
+let num1, num2, num3
+
+num1=num2=num3=2+2
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
