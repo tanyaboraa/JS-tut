7c1dee0336c13f9eb8b66aa32716586740b7b637
tanyaboraa <tanya.20243291@mnnit.ac.in>
2026-02-02 12:59:29 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,43 @@
+const name="tanya"
+// const repCount=50
+//  console.log(name+repoCount+"Value");
+// console.log(`Hello my name is ${name} and my repo count is ${repCount}`); //option+backtick
+// called string interpolation
+const gameName=new String('tanya-hc-com')
+// console.log(gameName); //key value pair, also length
+
+// console.log(gameName[0]);
+
+// console.log(gameName.__proto__); //{}
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// //methods ffrom mdn
+
+
+// const newString=gameName.substring(0,3);
+// console.log(newString);
+
+
+// const anotherString=gameName.slice(-8,4);
+// console.log(anotherString);
+
+
+const newStringOne="   tanya  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url="https://tanya.com/tanya%20bora"
+
+
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('tanya'));
+
+console.log(gameName.split('-'))
+
